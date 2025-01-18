1c28750ea9a2290d1fce7626245d0561a96fdd1a
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2025-01-18 11:15:02 -0300

    feat/ IsExpired
    adicionei um parâmetro que adiciona um limite de tempo para cada cache adicionado!

--- src/main/java/com/edev/looqbox/challange/Service/cacheServices/CacheServicesImpl.java
+++ src/main/java/com/edev/looqbox/challange/Service/cacheServices/CacheServicesImpl.java
@@ -1,7 +1,10 @@
 package com.edev.looqbox.challange.Service.cacheServices;
 
+import com.edev.looqbox.challange.Model.CacheItem;
 import com.edev.looqbox.challange.Model.PokemonDTO;
+
 import com.edev.looqbox.challange.Model.PokemonDTOWithHighlight;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -9,25 +12,39 @@ import java.util.Map;
 
 @Service
 public class CacheServicesImpl implements CacheServices{
-    private final Map<String, Object> inMemorieCache = new HashMap<>();
-    @Override
+
+    private final Map<String, CacheItem> cache = new HashMap<>();
+
+    @Autowired
     public void addtoCache(String key, PokemonDTO value) {
-        inMemorieCache.put(key, value);
-
+        cache.put(key, new CacheItem(value, System.currentTimeMillis()));
     }
-
-    @Override
+    @Autowired
     public PokemonDTO getFromCacheAsPokemonDTO(String key) {
-        return (PokemonDTO) inMemorieCache.get(key);
+        CacheItem cacheItem = cache.get(key);
+        if (cacheItem != null && isExpired(cacheItem)) {
+            return cacheItem.getPokemonDTO();
+        }
+        return null; // Retorna null se o item estiver expirado ou não existir
     }
 
     @Override
     public PokemonDTOWithHighlight getFromCacheAsPokemonDTOWithHighligth(String key) {
-        return (PokemonDTOWithHighlight) inMemorieCache.get(key);
+        CacheItem cacheItem = cache.get(key);
+        if (cacheItem != null && isExpired(cacheItem)) {
+            return cacheItem.getPokemonHighligthed();
+        }
+        return  null;
     }
 
-    @Override
-    public boolean isInCache(String key) {
-        return inMemorieCache.containsKey(key);
+    private boolean isExpired(CacheItem cacheItem) {
+        long cacheExpiryTime = 600000;
+        return (System.currentTimeMillis() - cacheItem.getTimestamp()) <= cacheExpiryTime;
     }
+    @Autowired
+    public boolean isInCache(String key) {
+        return getFromCacheAsPokemonDTO(key) != null;
+    }
+
+
 }
